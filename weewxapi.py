f4940cc1cb014a0ce00f0400963d7440f09113ef
piyo <piyo@nuc01.(none)>
2014-04-17 15:59:45 +0900

    template追加

--- weewxapi.py
+++ weewxapi.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, g
+from ConfigParser import SafeConfigParser
+import util
+
 app = Flask(__name__)
+app.debug = True
+
+#g.conf = SafeConfigParser().read("config.ini")
 
 @app.route("/")
-def hello():
-    print "aho"
-    return "Hello World!"
+def index():
+    
+    return render_template("index.html",)
 
 if __name__ == "__main__":
     app.run()
