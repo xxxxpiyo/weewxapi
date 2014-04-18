d5825cb259339a06641f4449c614f3b0009949ab
yukio.hasegawa <hasegawa@piyo.org>
2014-04-18 12:33:45 +0900

    index.html内の変数利用をFIX

--- weewxapi.py
+++ weewxapi.py
@@ -7,13 +7,21 @@ app.debug = True
 
 @app.before_request
 def before_request():
-  g.conf = SafeConfigParser().read("config.ini")
+  g.conf = SafeConfigParser()
+  g.conf.read("config.ini")
 
 
 @app.route("/")
 def index():
-    data["name"] = g.conf.station
-    return render_template("index.html",)
+    data = {}
+    data["name"] = g.conf.get("station", "name")
+    data["years"] = []
+    year = int(util.getDate(None, "%Y"))
+    while year >= int(g.conf.get("station", "firstyear")):
+        data["years"].append(str(year))
+        year = year - 1
+
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run()
